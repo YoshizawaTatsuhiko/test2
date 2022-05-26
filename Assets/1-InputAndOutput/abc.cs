6afe78fc7728af2bf099c7fdd6ed1e8fec390854
tatsupiko <identity2022@outlook.jp>
2022-05-26 13:31:58 +0900

    初めてのコミット

---
+++ Assets/1-InputAndOutput/abc.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abc : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Ç†ÇËÇ™Ç∆Ç§");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("ÉLÉÉÅ[");
+    }
+}
